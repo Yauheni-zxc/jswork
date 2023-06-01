9aab1e6a8a6332e7d5d98c85c883d53ec323de6c
Yauheni-zxc <117457997+Yauheni-zxc@users.noreply.github.com>
2023-06-01 15:01:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+//function araw(){
+//   m=  (document.getElementById("email").placeholder)
+//   
+
+//   document.getElementById("result").innerText="hello"+m
+// }
+function jem () 
+{var nam = prompt("Ваше имя?", "");
+ 
+
+alert("Привет - " + nam + "!");}
+jem (); 
